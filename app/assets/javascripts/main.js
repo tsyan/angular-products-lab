e07e9acde43e97139f847679d19d3d1cc8849912
Anna Tsykalova <tsyan@users.noreply.github.com>
2014-03-11 15:11:42 -0400

    added directives

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,11 +2,6 @@
 
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-//= require_self
-//= require_tree ./services/main
-//= require_tree ./filters/main
-//= require_tree ./controllers/main
-//= require_tree ./directives/main
 
 var StoreFront = angular.module('StoreFront',['ngRoute']);
 
