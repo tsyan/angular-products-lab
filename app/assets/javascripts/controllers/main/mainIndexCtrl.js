0893b478197303498f299e81c126fd234ad70c9d
Anna Tsykalova <tsyan@users.noreply.github.com>
2014-03-11 15:20:06 -0400

    began to get data remotely

--- app/assets/javascripts/controllers/main/mainIndexCtrl.js
+++ app/assets/javascripts/controllers/main/mainIndexCtrl.js
@@ -1,9 +1,6 @@
 var IndexCtrl = function($scope, $location, $http){
-    $scope.data = {};
-    $scope.data.products = [
-        {"name": "Shoe", "description": "Old Red smelly", "price": 0.22 },
-        {"name": "Frisbee", "description": "Silver with a gerbil taped to it" ,  "price": 7.63 }
-    ];
+   $scope.data = productData;
+    productData.loadProducts();
 
     // $scope.data = productData;
     // productData.loadProducts();
